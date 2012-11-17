238f6c4895a43b0002b7528baefa5eef845791ae
Monique Barbanson <mbarbanson@gmail.com>
2012-11-17 15:03:27 -0800

    Latest changes

--- Resources/lib/social.js
+++ Resources/lib/social.js
@@ -2,68 +2,68 @@
  * @author MONIQUE BARBANSON
  */
 
-
-var Contacts = require('lib/contacts');
-var FB = require('lib/facebook');
-
-function sharePhoto (photoBlob, thumbUrl, message) {
-	"use strict";
-	//var message = "Checkout this cool iPhone app that makes shopping more fun";
-	//FIXME move this behind a button
-	//Contacts.testContacts();
-	//FIXME move this behind a button
-	// log into facebook and link to external account on success
-	FB.authorize();
+(function () {
+	'use strict';
+	var Contacts = require('lib/contacts'),
+		FB = require('lib/facebook'),
+		Cloud = require('ti.cloud');
 	
-	// post to wall
-	FB.postPhoto(photoBlob, thumbUrl, message);
-	// call this to login with facebook instead of having fashionista specific credentials
-	//FB.linktoFBAccount();
-}
+	function sharePhoto (photoBlob, thumbUrl, message) {
 
-
-function chooseFBFriends () {
-	"use strict";
-	// log into facebook and link to external account unless we already have a valid access token
-	FB.authorize();
+		//var message = "Checkout this cool iPhone app that makes shopping more fun";
+		//FIXME move this behind a button
+		//Contacts.testContacts();
+		//FIXME move this behind a button
+		// log into facebook and link to external account on success
+		FB.authorize();
+		
+		// post to wall
+		FB.postPhoto(photoBlob, thumbUrl, message);
+		// call this to login with facebook instead of having fashionista specific credentials
+		//FB.linktoFBAccount();
+	}
 	
-	// get full list of FB friends
-	FB.getFriendsList();
-	// call this to login with facebook instead of having fashionista specific credentials
-	//FB.linktoFBAccount();
-}
-
-
-function findFBFriends (callback) {
-	"use strict";
-	// log into facebook and link to external account unless we already have a valid access token
-	FB.authorize();
 	
-	Cloud.SocialIntegrations.searchFacebookFriends(function (e){
-	    if (e.success) {
-	        if (e.users) {
-	        	Ti.API.info('Success:\\n' +
-	            	'Count: ' + e.users.length);
-	           }
-	        else {
-	        	Ti.API.info("Current user doesn;t have any friends using on Fashionista");
-	        }
-	        for (var i = 0; i < e.users.length; i++) {
-	            var user = e.users[i];
-	            Ti.API.info('id: ' + user.id + '\\n' +
-	                'first name: ' + user.first_name + '\\n' +
-	                'last name: ' + user.last_name);
-	         }
-	         return callback (e.users);
-	    } else {
-	        alert('Error:\\n' +
-	            ((e.error && e.message) || JSON.stringify(e)));
-            return null;
-	    }
-	});	
-}
+	function chooseFBFriends () {
 
+		// log into facebook and link to external account unless we already have a valid access token
+		FB.authorize();
+		
+		// get full list of FB friends
+		FB.getFriendsList();
+		// call this to login with facebook instead of having fashionista specific credentials
+		//FB.linktoFBAccount();
+	}
+	
+	
+	function findFBFriends (callback) {
 
-exports.sharePhoto = sharePhoto;
-exports.chooseFBFriends = chooseFBFriends;
-exports.findFBFriends = findFBFriends;
+		var i;
+		Cloud.SocialIntegrations.searchFacebookFriends(function (e){
+		    if (e.success) {
+		        if (e.users) {
+					Ti.API.info('Success:\\n' + 'Count: ' + e.users.length);
+		           }
+		        else {
+					Ti.API.info("Current user doesn;t have any friends using on Fashionista");
+		        }
+		        for (i = 0; i < e.users.length; i = i + 1) {
+		            var user = e.users[i];
+		            Ti.API.info('id: ' + user.id + '\\n' +
+		                'first name: ' + user.first_name + '\\n' +
+		                'last name: ' + user.last_name);
+		         }
+		         callback (e.users);
+		    } else {
+		        alert('Error:\\n' +
+		            ((e.error && e.message) || JSON.stringify(e)));
+		    }
+		});	
+	}
+	
+	// exported functions
+	exports.sharePhoto = sharePhoto;
+	exports.chooseFBFriends = chooseFBFriends;
+	exports.findFBFriends = findFBFriends;
+}) ();
+
--- Resources/ui/common/ShareWindow.js
+++ Resources/ui/common/ShareWindow.js
@@ -2,87 +2,232 @@
  * @author MONIQUE BARBANSON
  */
 
-var ThumbnailsWindow = require('ui/common/ThumbnailsWindow');
-var Contacts = require('lib/contacts');
-var FB = require('lib/facebook');
-
-function sharePhoto(photoUrl) {
-	"use strict";
-
-	Ti.API.info("sharePhoto: refresh the thumbnail window");
-	ThumbnailsWindow.refreshThumbnails();
-	Ti.API.info("This is where the user chooses who to share this image with");
-	//FIXME move this behind a button
-	Contacts.testContacts();
-	//FIXME move this behind a button
-	// log into facebook and link to external account on success
-	FB.authorize();
-	
-	// post to wall
-	FB.postToWall(photoUrl);
-	// call this to login with facebook instead of having fashionista specific credentials
-	//FB.linktoFBAccount();
-};
-
-function createShareWindow(photoUrl) {
-	"use strict";
-	var shareBtn,
+function createShareWindow(photoBlob, shareAction) {
+	'use strict';
+	var ApplicationTabGroup,
+		ListWindow,
+		social,
+		shareBtn,
+		cancelBtn,
 		shareWindow,
+		shareTabGroup,
+		tab,
 		caption,
 		shareLabel,
 		shareTable,
-		shareToFBFriends;
+		tableData,
+		shareToFBFriends,
+		shareToAddressBook,
+		inviteFBFriends,
+		inviteContacts,
+		followFBFriend;
 	
+	ApplicationTabGroup = require('ui/common/ApplicationTabGroup');
+	ListWindow = require('ui/common/ListWindow');
+	social = require('lib/social');
+	
+	// right nav button is Share
 	shareBtn = Titanium.UI.createButton({
 		style: Titanium.UI.iPhone.SystemButtonStyle.DONE,
-		title: L('share')
+		title: L('shareButton')
 	});
-	shareBtn.addEventListener('click', function (e) {
-											sharePhoto(photoUrl);
-											});
-	shareWindow = Ti.UI.createWindow({
-		backgroundColor: 'black',
-		barColor: '#5D3879',
-		rightNavButton: shareBtn,
-		title: L('share'),
-		tabBarHidden: true
+	cancelBtn = Titanium.UI.createButton({
+		style: Titanium.UI.iPhone.SystemButtonStyle.DONE,
+		title: L('cancel')
 	});
 	
-	caption = Ti.UI.createTextField({
-		hintText:L('add a caption here...'),
+	// share window										
+	shareWindow = Ti.UI.createWindow({
+		backgroundColor: '#ddd',
+		color: 'black',
+		barColor: '#5D3879',
+		rightNavButton: shareBtn,
+		leftNavButton: cancelBtn,
+		title: L('shareWindow')
+	});
+	
+	shareBtn.addEventListener('click', function (e) {
+											alert("calling sharePhoto. photoBlob = " + photoBlob);
+											social.sharePhoto(photoBlob, caption.value);
+											shareTabGroup.close();
+											});
+	cancelBtn.addEventListener('click', function (e) {
+											shareTabGroup.close();
+											});
+	//  crate a tab group with a single tab to hold the share window stack
+	shareTabGroup = ApplicationTabGroup.createApplicationTabGroup();
+	tab = Ti.UI.createTab({
+		icon: '/icons/light_grid.png',
+		window: shareWindow
+	});
+	shareWindow.containingTab = tab;
+	// hide tab Bar. We're just using a Tab Group to have a stack of windows without explicitly creating a navigation group which is an iOS only solution
+	shareWindow.setTabBarHidden(true);
+	shareTabGroup.addTab(tab);
+	shareTabGroup.open();
+	shareTabGroup.setVisible(true);
+	
+	// create share window elements
+	caption = Ti.UI.createTextArea({
+		value: L('Add a caption ...'),
+		color: '#aaa',
 		autocorrect: false,
 		autocapitalization: Titanium.UI.TEXT_AUTOCAPITALIZATION_NONE,
-		top:25,
+		top:10,
+		left: '5%',
 		width: '90%',
-		height: 40,
+		height: 100,
 		font: {
 			fontWeight: 'normal',
 			fontSize: '17'
 		},
-		textAlign: 'left',
-		color: '#333',
-		backgroundColor: '#ddd',
-		borderRadius: 3,
-		paddingLeft: 2, paddingRight: 2
+		textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+		verticalAlign: Ti.UI.TEXT_VERTICAL_ALIGNMENT_TOP,
+		wordWrap: true,
+		horizontalWrap: true,
+		borderRadius: 5,
+		paddingLeft: 2, 
+		paddingRight: 2,
+		backgroundColor: 'white',
+		//borderColor: 'black',
+		//borderWidth: 1
 	});
-	
 	shareWindow.add(caption);
 	
-	shareLabel = Ti.UI.createLabel({title: 'Choose friends from'});
-	shareTable = Ti.UI.createTableView();
+	caption.privHintText = caption.value;
+ 
+	caption.addEventListener('focus',function(e){
+	    if(e.source.value === e.source.privHintText){
+	        e.source.value = "";
+	        e.source.color = 'black';
+	    }
+	});
+	caption.addEventListener('blur',function(e){
+	    if(e.source.value ===""){
+	        e.source.value = e.source.privHintText;
+	        e.source.color = '#aaa';
+	    }
+	});
+	
+	shareLabel = Ti.UI.createLabel({
+		text: 'Choose friends to share with',
+		textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+		verticalAlign: Ti.UI.TEXT_VERTICAL_ALIGNMENT_TOP,
+		wordWrap: true,
+		color: 'black',
+		top: 120,
+		left: '5%',
+		height: 40,
+		width: '90%',
+		paddingLeft: 2, 
+		paddingRight: 2,
+		font: {
+			fontWeight: 'bold',
+			fontSize: '20'
+		}
+	});
+	shareWindow.add(shareLabel);
+	
+	shareTable = Ti.UI.createTableView({
+		top: 170,
+		height: Ti.UI.SIZE,
+		rowHeight: 50,
+		width: '90%',
+		left: '5%',
+		borderRadius: 5,
+		backgroundColor: 'transparent',
+		//borderColor: 'black',
+		//borderWidth: 1,
+		paddingLeft: 0, 
+		paddingRight: 2
+	});
+	
 	shareToFBFriends = Ti.UI.createTableViewRow({
-				title: 'facebook',
-				color: '#fff',
-				height:Ti.UI.SIZE
-			});
+				className: 'shareSource',
+				title: 'from facebook',
+				color: 'black',
+				backgroundColor: '#fff',
+				height: 40,
+				left: 0,
+				//borderWidth: 1,
+				//borderColor: 'black',
+				//leftImage: '/images/f_logo.png',
+				hasChild: true
+	});
+	
+	followFBFriend = function (friend) {
+			Ti.API.info("followFBFriend id: " + friend.id + " username " + friend.username);	
+	};
+	
+	shareToFBFriends.addEventListener('click', function (e) {
+		var callback = function (friends) {
+									var listWin;
+									if (friends && friends.length > 0) {
+										Ti.API.info("create and populate list of friends window");
+										listWin = ListWindow.populateList(friends, followFBFriend);
+										listWin.containingTab = tab;
+										tab.open(listWin);
+									}
+									else {
+										Ti.API.info('Current user is the first in their social network to use Fashionista! Kudos!');
+									}
+					},
+		//facebook integration
+			FB = require('lib/facebook'),
+			authCB = function () { 
+				Ti.API.info ("facebook authorize callback");
+				social.findFBFriends (callback); 
+			};
+		FB.authorize(authCB);								
+	});
+	
 	shareToAddressBook = Ti.UI.createTableViewRow({
-				title: 'address book',
-				color: '#fff',
-				height:Ti.UI.SIZE
-			});
+				className: 'shareSource',		
+				title: 'from your contacts',
+				color: 'black',
+				backgroundColor: '#fff',
+				top: 50,
+				height: 40,
+				left: 0,
+				//borderWidth: 1,
+				//borderColor: 'black',
+				//leftImage: '/images/contacts-medium.png',								
+				hasChild: true
+	});
+	
+	inviteFBFriends = Ti.UI.createTableViewRow({
+				className: 'shareSource',		
+				title: 'invite via facebook',
+				color: 'black',
+				backgroundColor: '#fff',				
+				top: 100,
+				height: 40,
+				left: 0,
+				//borderWidth: 1,
+				//borderColor: 'black',
+				//leftImage: '/images/f_logo.png',				
+				hasChild: true
+	});
+	inviteContacts = Ti.UI.createTableViewRow({
+				className: 'shareSource',		
+				title: 'invite via text',
+				color: 'black',
+				backgroundColor: '#fff',				
+				top: 150,
+				height: 40,
+				left: 0,
+				//borderWidth: 1,
+				//borderColor: 'black',	
+				//leftImage: '/images/contacts-medium.png',							
+				hasChild: true
+	});
 	shareTable.appendRow(shareToFBFriends);
 	shareTable.appendRow(shareToAddressBook);
-
+	shareTable.appendRow(inviteFBFriends);
+	shareTable.appendRow(inviteContacts);
+	shareWindow.add(shareTable);
+	
+	return shareWindow;
 }
 
 exports.createShareWindow = createShareWindow;
